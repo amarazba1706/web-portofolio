81752d31e61c149d875821c750efa5397585c8cd
Amar Azmi Baihaqi <128926833+amarazba1706@users.noreply.github.com>
2023-04-07 00:01:47 +0700

    amar
    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Tukang Balbalan", "Hamba Allah"],
+  typeSpeed: 10,
+  backSpeed: 10,
+  backDelay: 100,
+  loop: true,
+});
